9a1973302d5cefe8e132ae40c69707d258721c16
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-08-17 15:08:27 +0530

    index.html

---
+++ switchcase/app.js
@@ -0,0 +1,18 @@
+let day="3";
+switch(day){
+    case "1":console.log("monday");
+    break;
+    case "2":console.log("tuesday");
+    break;
+    case "3":console.log("wednesday");
+    break;
+    case "4":console.log("thursday");
+    break;
+    case "5":console.log("friday");
+    break;
+    case "6":console.log("saturday");
+    break;
+    case "7":console.log("sunday");
+    break;
+    default:console.log("day are not pressent")
+}

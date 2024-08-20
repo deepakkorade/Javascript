3efb2292a0224d55e2e849e8d3ad7f35ef7c6e26
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-08-20 18:32:19 +0530

    practice.html

---
+++ logicalquestion/practice.js
@@ -0,0 +1,8 @@
+                                 //question first!
+// let msg="hello!";
+// console.log(msg.trim().toUpperCase());/
+                                  //question second
+// let name="apanacollege";
+// let nameone=name.slice(5,11);
+// console.log(nameone);
+                                 //question third

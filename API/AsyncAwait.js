9722d88709331b5093f45613cc88e1baad7803c7
deepak korade <147805353+deepakkorade@users.noreply.github.com>
2024-09-01 08:33:43 +0530

    await.js

---
+++ API/AsyncAwait.js
@@ -0,0 +1,19 @@
+let url = "https://catfact.ninja/fact";
+
+async function getfacts(){
+      try{
+        let res=await  fetch(url);
+      let data = await res.json();
+      console.log(data.fact);
+
+      let res2=await  fetch(url);
+      let data2 = await res2.json();
+      console.log(data2.fact);
+
+      }catch(err){
+         console.log("error",err);
+      }
+    }
+
+
+getfacts();
